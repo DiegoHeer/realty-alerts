2c495ac0df2d0f836684af496ccb8203cb9b973a
Diego Heer <diegojonathanheer@gmail.com>
2025-06-20 19:52:02 +0000

    Models|Add: created initial pydantic models to validate imported realty queries

---
+++ src/enums.py
@@ -0,0 +1,12 @@
+from enum import StrEnum
+
+
+class Websites(StrEnum):
+    FUNDA = "funda"
+
+
+class HouseTypes(StrEnum):
+    WOONHUIS = "Woonhuis"
+    APPARTEMENT = "Appartement"
+    PARKEERGELEGENHEID = "Parkeergelegenheid"
+    BOUWGROND = "Bouwgrond"
---
+++ src/models.py
@@ -0,0 +1,25 @@
+from cron_validator import CronValidator
+from pydantic import BaseModel, field_validator
+
+from enums import HouseTypes, Websites
+
+
+class RealtyFilters(BaseModel):
+    house_type: HouseTypes
+
+
+class RealtyQuery(BaseModel):
+    cron_schedule: str
+    website: Websites
+    filters: RealtyFilters
+
+    @field_validator("cron_schedule")
+    @classmethod
+    def validate_cron(cls, value: str) -> str:
+        try:
+            CronValidator.parse(value)
+        except ValueError:
+            msg = f"The cron schedule {value} is invalid. Please place a valid crontab."
+            raise ValueError(msg)
+
+        return value
