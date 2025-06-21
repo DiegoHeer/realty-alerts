14a1941bb3cfd6b6908d967a0d4a4d1b53327e12
Diego Heer <diegojonathanheer@gmail.com>
2025-06-21 01:32:04 +0000

    Storage|Add: created an ORM model for query results

---
+++ src/storage.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from peewee import CharField, Check, DateTimeField, Model, SqliteDatabase
+
+from enums import QueryResultORMStatus
+
+sqlite_db = SqliteDatabase("db.sqlite", pragmas={"journal_mode": "wal"})
+
+
+class QueryResultsORM(Model):
+    url = CharField(unique=True)
+    title = CharField()
+
+    created_at = DateTimeField(default=datetime.now)
+    updated_at = DateTimeField(default=datetime.now)
+    status = CharField(
+        choices=QueryResultORMStatus.choices(),
+        default=QueryResultORMStatus.NEW,
+        constraints=[Check(f"status IN {tuple(QueryResultORMStatus.values())}")],
+    )
+
+    def save(self, *args, **kwargs):
+        self.updated_at = datetime.now()
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        database = sqlite_db
+        table_name = "query_results"
