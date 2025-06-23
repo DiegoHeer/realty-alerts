e8654c8ef8acab89e656688a66188a2b499f0054
Diego Heer <diegojonathanheer@gmail.com>
2025-06-23 18:28:44 +0000

    CeleryConfig|Refactor: moved the celery config to a separate file, to avoid circular dependencies

--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,6 @@
 from pathlib import Path
-from typing import Any
 
-from celery.schedules import crontab
 from pydantic_settings import BaseSettings
-
-from models import RealtyQuery
 
 
 class Settings(BaseSettings):
@@ -15,26 +11,3 @@ class Settings(BaseSettings):
 
 SETTINGS = Settings()
 DATA_PATH = Path(__file__).resolve().parents[1] / "data"
-
-
-class CeleryConfig:
-    broker_url = SETTINGS.redis_url
-    result_backend = SETTINGS.redis_url
-    timezone = SETTINGS.timezone
-    enable_utc = True
-    beat_schedule_filename = str(DATA_PATH / "celerybeat-schedule")
-
-    def __init__(self, realty_queries: list[RealtyQuery]) -> None:
-        self.beat_schedule = get_beat_schedule(realty_queries)
-
-
-def get_beat_schedule(realty_queries: list[RealtyQuery]) -> dict[str, dict]:
-    return {str(iterable): _build_query_schedule(query) for iterable, query in enumerate(realty_queries)}
-
-
-def _build_query_schedule(query: RealtyQuery) -> dict[str, Any]:
-    return {
-        "task": "tasks.main",
-        "schedule": crontab.from_string(crontab=query.cron_schedule),
-        "args": [query.model_dump()],
-    }
