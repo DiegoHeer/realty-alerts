858debcbfe438a5a97cfe42e3aa57bf42e2e7df7
Diego Heer <diegojonathanheer@gmail.com>
2025-06-30 23:39:03 +0000

    Models/RealtyQuery|Refactor: removed max restriction on listing page numbers

--- src/models.py
+++ src/models.py
@@ -15,7 +15,7 @@ class RealtyQuery(BaseModel):
     ntfy_topic: str
     cron_schedule: str
     query_url: str
-    max_listing_page_number: Annotated[int, Field(strict=True, ge=0, le=5)] = 3
+    max_listing_page_number: Annotated[int, Field(strict=True, ge=0)] = 3
     notify_if_no_new_listing: bool = False
 
     @property
