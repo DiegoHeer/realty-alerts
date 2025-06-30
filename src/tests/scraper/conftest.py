c537e9ab3d9f84625eb803560b2075f1651e5a27
Diego Heer <diegojonathanheer@gmail.com>
2025-06-30 23:42:33 +0000

    Tests/Conftest|Refactor: moved mocking logic to common file, to be used by all scraper tests

---
+++ src/tests/scraper/conftest.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+MOCK_DATA_DIR = Path(__file__).resolve().parent / "data"
+
+
+def mock_get_url_content(url: str) -> str:
+    url_to_file_name = {
+        "https://www.funda.nl/zoeken/koop?object_type=%5B%22house%22,%22apartment%22%5D": "funda_listing",
+        "https://www.funda.nl/zoeken/koop?object_type=%5B%22house%22%2C%22apartment%22%5D&search_result=1": "funda_1",
+        "https://www.funda.nl/zoeken/koop?object_type=%5B%22house%22%2C%22apartment%22%5D&search_result=2": "funda_2",
+        "https://www.funda.nl/detail/koop/blokker/huis-bangert-58/43063167/": "funda_detail",
+        "https://www.funda.nl/scraper-detected": "funda_scraper_detected",
+        "https://www.pararius.nl/koopwoningen/nederland/50m2": "pararius_listing",
+        "https://www.pararius.nl/koopwoningen/nederland/50m2/page-1": "pararius_listing",
+        "https://www.pararius.nl/scraper-detected": "pararius_scraper_detected",
+    }
+
+    try:
+        return _load_html_file(file_name=f"{url_to_file_name[url]}.html")
+    except KeyError:
+        raise KeyError(f"No mock defined for URL: {url}")
+
+
+def _load_html_file(file_name: str) -> str:
+    file_path = MOCK_DATA_DIR / file_name
+    return file_path.read_text(encoding="utf-8")
