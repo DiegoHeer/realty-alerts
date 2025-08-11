a058410eb6d348a87f238505aa63b8b68a6b6561
Diego Heer <diegojonathanheer@gmail.com>
2025-08-11 21:04:19 +0000

    Django/Admin|Add: created admin configuration for managing the new Django models

---
+++ src/ui/admin.py
@@ -0,0 +1,56 @@
+from django.contrib import admin
+
+from ui.models import RealtyQuery, RealtyResult
+
+
+@admin.register(RealtyQuery)
+class RealtyQueryAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "cron_schedule",
+        "website",
+        "notify_startup_of_app",
+        "notify_if_no_new_listing",
+    )
+    list_filter = (
+        "notify_startup_of_app",
+        "notify_if_no_new_listing",
+    )
+    search_fields = ("name", "ntfy_topic", "query_url")
+    readonly_fields = ("website", "notification_url")
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "name",
+                    "ntfy_topic",
+                    "cron_schedule",
+                    "query_url",
+                    "max_listing_page_number",
+                    "notify_startup_of_app",
+                    "notify_if_no_new_listing",
+                )
+            },
+        ),
+        (
+            "Computed Fields",
+            {
+                "fields": ("website", "notification_url"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+
+@admin.register(RealtyResult)
+class RealtyResultAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "price",
+        "query",
+    )
+    list_filter = ("query",)
+    search_fields = ("title", "price", "query__name")
+    list_select_related = ("query",)
