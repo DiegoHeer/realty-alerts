8b292fc29f7e20b8beae0d6d369e1c8fd1088c98
Diego Heer <diegojonathanheer@gmail.com>
2025-08-26 18:19:27 +0000

    Django/Dev|Add: automatically refresh the browser when making changes in development

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django_celery_beat",
     "django_celery_results",
     "django_tailwind_cli",
+    "django_browser_reload",
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
