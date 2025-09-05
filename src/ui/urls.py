1058d59610d389646664ffed84be04030f3db312
Diego Heer <diegojonathanheer@gmail.com>
2025-09-05 22:31:21 +0000

    Urls|Refactor: updated the urls, to make them more consistent

--- src/ui/urls.py
+++ src/ui/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from ui.views import RealtyQueryListView, RealtyQueryDetailView, HomeView
+from ui.views import RealtyQueryListView, RealtyQueryDetailView, HomeView, RealtyResultsListView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path("query-list/", RealtyQueryListView.as_view(), name="realty-query-list"),
-    path("query/<int:pk>", RealtyQueryDetailView.as_view(), name="realty-query-detail"),
+    path("queries/list", RealtyQueryListView.as_view(), name="realty-query-list"),
+    path("queries/<int:pk>", RealtyQueryDetailView.as_view(), name="realty-query-detail"),
+    path("queries/<int:pk>/results/", RealtyResultsListView.as_view(), name="realty-result-list"),
 ]
